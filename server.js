d4cc49e8a27dcd8607133d6dff01a62b2ad96cde
Chris <chrisnoshad52@gmail.com>
2025-06-24 21:01:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware - Allow all CORS for development
-app.use(cors({ origin: '*' }));
+// Add CORS headers globally
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
